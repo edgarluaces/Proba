65e2ea5675917bd09451c35afb0e3c377543442c
Edgar Luaces Roig <edgar_luaces@ceroca.cat>
2024-09-19 19:32:16 +0200

    bordes

--- Assets/Scripts/Nuevo.cs
+++ Assets/Scripts/Nuevo.cs
@@ -6,10 +6,19 @@ using UnityEngine;
 public class Nuevo : MonoBehaviour
 {
     private float velocitat;
+    private Vector2 minPantalla, maxPantalla;
+
     // Start is called before the first frame update
     void Start()
     {
         velocitat = 8;
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+
+        minPantalla.x = minPantalla.x = + 0.75f;
+        maxPantalla.x = maxPantalla.x = - 0.75f;
+        minPantalla.y = minPantalla.y = + 0.75f;
+        maxPantalla.y = maxPantalla.y = - 0.75f;
     }
 
     // Update is called once per frame
@@ -21,9 +30,10 @@ public class Nuevo : MonoBehaviour
         //Debug.Log("X: " + direccioIndicadaX + " - Y: " + direccioIndicadaY);
 
         Vector2 direccioindicada = new Vector2(direccioIndicadaX, direccioIndicadaY);
-
         Vector2 novaPosicio = transform.position;
         novaPosicio = novaPosicio + direccioindicada * velocitat * Time.deltaTime;
+        novaPosicio.x = Mathf.Clamp(novaPosicio.x, minPantalla.x, maxPantalla.x);
+        novaPosicio.y = Mathf.Clamp(novaPosicio.y, minPantalla.y, maxPantalla.y);
 
         transform.position = novaPosicio;
     }
