1bbc514fd08c93a4b4d12fb182500a963af1952d
Edgar Luaces Roig <edgar_luaces@ceroca.cat>
2024-09-23 17:04:34 +0200

    ultimo

--- Assets/Scripts/Nuevo.cs
+++ Assets/Scripts/Nuevo.cs
@@ -15,15 +15,20 @@ public class Nuevo : MonoBehaviour
         minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
         maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
 
-        minPantalla.x = minPantalla.x + 0.75f;
-        maxPantalla.x = maxPantalla.x - 0.75f;
-        minPantalla.y = minPantalla.y + 0.75f;
-        maxPantalla.y = maxPantalla.y - 0.75f;
+        minPantalla.x += GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+
+
+        //bordes para que la nave no salga
+        minPantalla.x = minPantalla.x + 0.50f;
+        maxPantalla.x = maxPantalla.x - 0.50f;
+        minPantalla.y = minPantalla.y + 0.50f;
+        maxPantalla.y = maxPantalla.y - 0.50f;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        //movimiento izquierda y derecha
         float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
         float direccioIndicadaY = Input.GetAxisRaw("Vertical");
 
